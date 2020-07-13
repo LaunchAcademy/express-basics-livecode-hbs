01b44e27385c36fa3adfe16790d04c69b4ff12ae
Patrick Whelan <patrickawhelan@gmail.com>
2020-07-13 18:09:28 -0400

    final code for A3

--- src/app.js
+++ src/app.js
@@ -20,7 +20,40 @@ app.set('view engine', 'hbs')
 
 
 //your code here
+app.get('/',(req,res) => {
+  res.redirect('/greeting')
+})
 
+app.get('/greeting',(req,res) => {
+  res.render('index.hbs')
+})
+
+app.get('/greeting/:name', (req,res) => {
+  let lang=req.query.lang
+  let name = req.params.name
+  if (lang && lang.toLowerCase() === 'fr'){
+    res.render('show.hbs',{name:name, greeting:'Bonjour Patrice'})
+  } else if (lang === 'es') {
+    res.render('show.hbs', {name:name, greeting:'Hola, Paco'})
+  } else if (lang && (lang !== 'fr' && lang !== 'es')) {
+    res.status(404).send("We don't currently support that language")
+  }
+   else {
+    res.render('show.hbs', {name:name, greeting:"Hey there"})
+  }
+})
+
+app.get('/favorite-colors/:name', (req,res) => {
+  let name = req.params.name
+  let favoriteColor
+  if (name === "Sam"){
+    favoriteColor = "Red"
+  } else if (name === "John") {
+    favoriteColor = "Green"
+  }
+
+  res.send(`<h2>${name}'s favorite color is ${favoriteColor}`)
+})
 
 app.listen(3000, '0.0.0.0', () => {
   console.log('Server is listening...')
