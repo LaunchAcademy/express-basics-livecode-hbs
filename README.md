73d5b0f4a55d6601e81fd19f9947b17a8ef67161
Patrick Whelan <patrickawhelan@gmail.com>
2020-07-13 15:35:51 -0400

    removed code for fresh slate

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # express-basics-livecode-hbs
+
+Code through the frist three stories in your-first-dynamic-web-app-with-express
+Add handlebars for index and show
--- src/app.js
+++ src/app.js
@@ -20,31 +20,7 @@ app.set('view engine', 'hbs')
 
 
 //your code here
-app.get('/', (req, res) => {
-  res.redirect('/geeting')
-})
 
-app.get('/greeting', (req,res) => {
-  res.render('index.hbs')
-})
-
-app.get('/greeting/:name', (req,res) => {
-  let name = req.params.name 
-  let language = req.query.lang
-  let greeting
-  if (language && language.toLowerCase() ==="fr") {
-    greeting = "bonjour"
-    res.render(`show.hbs`, {name, greeting})
-  }
-  else if (language && language.toLowerCase() === "es"){
-    greeting = "Hola"
-    res.render(`show.hbs`,{ name, greeting })
-  } else if (language) {
-    res.status(404).send("We don't support that language")
-  } else {
-    res.render('show.hbs', {name:name, greeting:"Hey There"})
-  }
-})
 
 app.listen(3000, '0.0.0.0', () => {
   console.log('Server is listening...')
