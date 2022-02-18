84fc84c76367dac7cd371c6a73cb9ef219bc3f89
Martin Serna <martin.serna@launchacademy.co>
2022-02-18 16:57:13 -0500

    Final code

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -7,7 +7,14 @@ rootRouter.get("/", (req, res) => {
 })
 
 // your code here
+rootRouter.get("/greetings", (req, res) => {
+  res.render("index.hbs")
+})
 
+rootRouter.get("/greetings/:name", (req, res) => {
+  const name = req.params.name
+  res.render("show.hbs", { name })
+})
 
 
 export default rootRouter
